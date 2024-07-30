3c940f3ac0663910bd7e45181f50b00941002b38
Babiker Babiker <157412784+BabikerBabiker@users.noreply.github.com>
2024-07-29 23:56:30 -0700

    updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/Pantry-App',
-    assetPrefix: '/Pantry-App/',
+    basePath: '',
+    assetPrefix: '',
 };
   
 export default nextConfig;
